08ca85545fa0d2ccf32c9fb2fa40e647ba49ebb5
Varu <142126351+olenarokalo@users.noreply.github.com>
2024-09-14 20:51:43 +0100

    lil bug fix

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,7 +4,7 @@ import css from "./Loader.module.css";
 export default function Loader() {
   return (
     <div className={css.wrap}>
-      <BounceLoader color="#646cff" />
+      <BounceLoader color="#d1f0e1" />
     </div>
   );
 }
